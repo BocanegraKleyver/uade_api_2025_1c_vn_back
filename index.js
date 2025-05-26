97c86358da27f034d0933c2818661f8e35a66990
BocanegraKleyver <kbocanegracisneros@uade.edu.ar>
2025-05-25 22:33:34 -0300

    Backend inicial  conexión a MongoDB Atlas

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando 👨‍🍳");
+});
+
+const PORT = process.env.PORT || 3001;
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Conectado a MongoDB Atlas");
+    app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+  })
+  .catch((err) => console.error("Error conectando a MongoDB", err));
